93ffad7d343b5e9009201d3fd6e7c53e6afea4ee
Jayden <jayden@pop-os.localdomain>
2021-09-29 22:51:10 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Egg Latin Translator" + "\n------------------------------" + "\nInput: ");
+        String s = in.nextLine();
+        String egg = ("");
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == 'A' || s.charAt(i) == 'a' ||
+                    s.charAt(i) == 'E' || s.charAt(i) == 'e' ||
+                    s.charAt(i) == 'I' || s.charAt(i) == 'i' ||
+                    s.charAt(i) == 'O' || s.charAt(i) == 'o' ||
+                    s.charAt(i) == 'U' || s.charAt(i) == 'u' ||
+                    s.charAt(i) == 'Y' || s.charAt(i) == 'y') {
+
+                egg = egg + "egg" + s.charAt( i );
+            }
+
+            else {
+
+                egg = egg + s.charAt( i );
+            }
+        }
+
+        System.out.println("------------------------------" + "\nOutput: " + egg);
+
+    }
+}
